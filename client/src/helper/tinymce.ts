6f0c6500c69083f3bbf1beb6c0bd773a7ea8c7b1
Ethan <trannguyendanhuy2904@gmail.com>
2023-11-24 16:34:12 +0700

    third commit

--- client/src/helper/tinymce.ts
+++ client/src/helper/tinymce.ts
@@ -1,11 +1,11 @@
 import tinymce from "tinymce";
 
 export const attacthTinymce = () => {
-    const availableContentEdit = {
-        selector: '.tinymce-body',
+    const tinymceEditConfig = {
+        selector: '.tinymce-edit',
         menubar: false,
         inline: true,
-        plugins: ' mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste   typography inlinecss',
+        plugins: ' mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist  casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste   typography inlinecss',
         toolbar: [
             ' fontfamily fontsize | bold italic underline | emoticons',
             'forecolor backcolor | alignleft aligncenter alignright alignfull lineheight | checklist numlist bullist outdent indent'
@@ -13,6 +13,8 @@ export const attacthTinymce = () => {
         valid_styles: {
             '*': 'font-size,font-family,color,text-decoration,text-align'
         },
+        paste_block_drop: false,
+        paste_data_images: true,
         powerpaste_word_import: 'clean',
         powerpaste_html_import: 'clean',
     };
@@ -21,33 +23,38 @@ export const attacthTinymce = () => {
         selector: '.tinymce-heading',
         menubar: false,
         inline: true,
-        plugins: ' mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste   typography inlinecss',
+        plugins: ' mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist  casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste   typography inlinecss',
         toolbar: ' fontfamily fontsize | bold italic underline  | emoticons',
         valid_styles: {
             '*': 'font-size,font-family,color,text-decoration,text-align'
         },
+
         powerpaste_word_import: 'clean',
         powerpaste_html_import: 'clean',
     };
 
-
-    tinymce.init({
-        selector: '.tinymce-edit',
+    const tinymceCreateConfig = {
+        selector: '.tinymce-create',
         menubar: false,
         // inline: true,
+        statusbar: false,
         toolbar: [
             ' fontfamily fontsize | bold italic underline | emoticons',
-            'forecolor backcolor | alignleft aligncenter alignright alignfull lineheight | checklist numlist bullist outdent indent'
+            'forecolor backcolor | alignleft aligncenter alignright alignfull lineheight | checklist numlist bullist outdent indent | link image media  table '
         ],
-        plugins: '  mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks  checklist mediaembed casechange export formatpainter pageembed permanentpen  advtemplate advtable advcode editimage tableofcontents mergetags powerpaste   typography inlinecss',
+        plugins: 'mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks  checklist  casechange export formatpainter pageembed permanentpen  advtemplate advtable advcode editimage tableofcontents mergetags powerpaste  typography inlinecss',
         // toolbar: '| blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight |  checklist numlist bullist indent outdent | emoticons forecolor backcolor  | ',
         valid_styles: {
             '*': 'font-size,font-family,color,text-decoration,text-align'
         },
+        paste_block_drop: false,
+        paste_data_images: true,
         powerpaste_word_import: 'clean',
         powerpaste_html_import: 'clean',
-    });
+    }
 
-    tinymce.init(emailHeaderConfig);
-    tinymce.init(availableContentEdit);
+
+
+    tinymce.init(tinymceCreateConfig);
+    tinymce.init(tinymceEditConfig);
 }
